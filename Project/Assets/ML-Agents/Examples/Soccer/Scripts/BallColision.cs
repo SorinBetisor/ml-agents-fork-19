45d50c2798bc6d421fcde8667b3954be1671eec2
Jonas Tuukkanen <161715368+JonasTuukkanen@users.noreply.github.com>
2024-10-31 15:24:40 +0100

    Add files via upload

---
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/BallColision.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallColision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "purpleAgent" || col.gameObject.tag == "blueAgent" || col.gameObject.tag == "wall"){
+            Debug.Log("sound");
+        }
+    }
+}
